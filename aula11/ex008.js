e81460bc443567ae1e1e8356cbe7ee243c4b56a1
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-11-25 15:00:40 -0300

    js
    ex009

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 60.5
+var vel = 66.9
 console.log(`A velocidade de seu carro é ${vel}Km/h`)
-if (vel > 60){
+if (vel > 60){ //Condição simples!
     console.log(`você passou a velocidade. Multado`)
 }
 console.log(`Dirija sempre com segurança`)
