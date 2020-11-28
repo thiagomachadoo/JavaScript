124bc3a5441c932729570c00b110200713957aee
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-11-28 16:01:49 -0300

    new ex

--- aula11/ex0011.js
+++ aula11/ex0011.js
@@ -1,9 +1,11 @@
-var idade = 23
+var idade = 17
  if (idade < 16) {
-    console.log('Menor de idade')
+        console.log('Menor de idade')
 } else {
     if ( idade < 18){
         console.log('Voto opicional')
+    }else{
+        console.log('Voto obrigatório')
     }
 }
    
