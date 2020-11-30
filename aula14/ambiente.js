4b46b56cbbac068429c3a9210bfdd9ad69f75065
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-11-30 20:24:51 -0300

    Update ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,11 +1,5 @@
-/*
-var c = 1
-while (c <= 5) {
-    console.log(c)
-    c++
-}
-//-----------------------
-*/
-for(var c = 1;c <=9;c++) {
+console.log('Vai começar...')
+for(var c = 1;c r<=9;c++) {
     console.log(c)
 }
+console.log('Fim')
