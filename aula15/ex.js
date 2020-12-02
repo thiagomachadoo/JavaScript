df85a3cb53fa0dcb96701e1a8ee9fff44e483134
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-12-01 22:11:25 -0300

    teste02

--- aula15/ex.js
+++ aula15/ex.js
@@ -1,2 +1,10 @@
 let num = [5,6,7,8,9,10]
-console.log(`CADE O VETOR DE ${num}`)
+num.push(1)
+num.sort()
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor é ${num[0]}`)
+
+
+
+//num.length comprimento do vetor
+//num.sort() coloca os elementos em ordem crescente
