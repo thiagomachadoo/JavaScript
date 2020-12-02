e9bb5d64832302399c96838c86fa9ec03e0ef178
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-12-01 22:00:16 -0300

    Update ambiente.js

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -3,3 +3,4 @@ for(var c = 1;c <=9;c++) {
     console.log(c)
 }
 console.log('Fim')
+//um array ou um vetor é uma variavel composta é uma variavel que tem varios e elementos, e por seguinte esses elementos é composto por um valor e por uma chave de verificação
