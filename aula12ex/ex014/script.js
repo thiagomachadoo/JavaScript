74ecea7cbf0a0c4e564aed75482420711d9465eb
Thiago Machado <thiagodasilvamachadoo44@gmail.com>
2020-12-05 22:22:15 -0300

    horas sincronizadas

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,2 +1,14 @@
-var msg = document.getElementById('msg')
-var img = document.getElementById('imagem')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora >= 0 && hora < 22) {
+        //BOM DIA!
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE!
+    } else {
+        //BOA NOITE!
+    }
+}
